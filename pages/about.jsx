2d5d92e1feece07fa7fe5094d00519090add88f3
deekshithmd <deekshithmogra@gmail.com>
2023-04-03 10:50:11 +0530

    completed about

--- pages/about.jsx
+++ pages/about.jsx
@@ -1,5 +1,5 @@
 import styled from "styled-components";
-import { H1, H3 } from "@/styles/SharedStyling";
+import { H1, H3, Span } from "@/styles/SharedStyling";
 import Link from "next/link";
 import Image from "next/image";
 
@@ -9,20 +9,20 @@ const About = () => {
       <Header>
         <span style={{ color: "#f9ca24" }}>About</span> me
       </Header>
-      <div>
-        <div>
-          <H3>
-            <span>name:</span>Deekshith M D
-          </H3>
-          <H3>
-            <span>age:</span>24
-          </H3>
-          <H3>
-            <span>qualification:</span>B.E(CSE)
-          </H3>
-          <H3>
-            <span>post:</span>front end developer
-          </H3>
+      <DataRow>
+        <Info>
+          <Field>
+            <FieldName>name:</FieldName>Deekshith M D
+          </Field>
+          <Field>
+            <FieldName>age:</FieldName>24
+          </Field>
+          <Field>
+            <FieldName>qualification:</FieldName>B.E(CSE)
+          </Field>
+          <Field>
+            <FieldName>post:</FieldName>front end developer
+          </Field>
           <Link href="/files/Deekshith_M_D.pdf" download>
             <Button>
               Resume
@@ -35,18 +35,42 @@ const About = () => {
               />
             </Button>
           </Link>
-        </div>
-        <div>
+        </Info>
+        <BoxContainer>
           <Box>
-            <span>2</span>
-            <H3>Years of experience</H3>
+            <Span fontSize="4rem" color="#f9ca24">
+              2
+            </Span>
+            <H3 fontSize="2rem" color="#f9ca24">
+              Years of experience
+            </H3>
           </Box>
           <Box>
-            <span>10+</span>
-            <H3>Projects</H3>
+            <Span fontSize="4rem" color="#f9ca24">
+              10+
+            </Span>
+            <H3 fontSize="2rem" color="#f9ca24">
+              Projects
+            </H3>
           </Box>
-        </div>
-      </div>
+          <Box>
+            <Span fontSize="4rem" color="#f9ca24">
+              10+
+            </Span>
+            <H3 fontSize="2rem" color="#f9ca24">
+              Projects
+            </H3>
+          </Box>
+          <Box>
+            <Span fontSize="4rem" color="#f9ca24">
+              10+
+            </Span>
+            <H3 fontSize="2rem" color="#f9ca24">
+              Projects
+            </H3>
+          </Box>
+        </BoxContainer>
+      </DataRow>
     </Container>
   );
 };
@@ -58,6 +82,9 @@ const Header = styled(H1)`
   padding: 1rem;
   border-bottom: 0.1rem solid #fff4;
   color: #fff;
+  @media (max-width: 480px) {
+    margin: 0 3rem;
+  }
 `;
 
 const Container = styled.div`
@@ -66,7 +93,7 @@ const Container = styled.div`
   display: flex;
   justify-content: center;
   flex-flow: column;
-  padding: 0 15rem;
+  padding: 0 2rem;
   background: #111;
   @media (max-width: 981px) {
     margin-left: 0;
@@ -89,6 +116,48 @@ const Button = styled.button`
   }
 `;
 
-const Box = styled.div``;
+const Box = styled.div`
+  width: 15rem;
+  background: #222;
+  text-align: center;
+  padding: 2rem;
+  margin: 2rem;
+  @media (max-width: 480px) {
+    width: 100%;
+  }
+`;
+
+const DataRow = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-wrap: wrap;
+  padding: 1rem 0;
+`;
+
+const Info = styled.div`
+  flex: 1 1 48rem;
+  padding: 2rem 1rem;
+  padding-left: 6rem;
+`;
+
+const Field = styled(H3)`
+  font-size: 2rem;
+  color: #eee;
+  padding: 1rem 0;
+  font-weight: normal;
+`;
+
+const FieldName = styled.span`
+  color: #f9ca24;
+  padding: 0 0.5rem;
+`;
+
+const BoxContainer = styled.div`
+  flex: 1 1 48rem;
+  display: flex;
+  justify-content: center;
+  flex-wrap: wrap;
+`;
 
 export default About;
--- styles/SharedStyling.js
+++ styles/SharedStyling.js
@@ -4,10 +4,18 @@ export const H1 = styled.h1`
   font-size: ${(props) => props.fontSize || "16px"};
   font-weight: ${(props) => props.fontWeight || "400"};
   color: ${(props) => props.color || "#000"};
+  padding: ${(props) => props.padding || "0px"};
 `;
 
 export const H3 = styled.h3`
   font-size: ${(props) => props.fontSize || "16px"};
   font-weight: ${(props) => props.fontWeight || "400"};
+  color: ${(props) => props.color || "#000"};
+  padding: ${(props) => props.padding || "0px"};
+`;
+
+export const Span = styled.span`
+  padding: ${(props) => props.padding || "0px"};
+  font-size: ${(props) => props.fontSize || "16px"};
   color: ${(props) => props.color || "#000"};
 `;
