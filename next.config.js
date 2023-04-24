5c7f527b20bf1a3fe0a55269e2fe56be01c462e7
deekshithmd <deekshithmogra@gmail.com>
2023-04-24 11:09:53 +0530

    updated config

--- next.config.js
+++ next.config.js
@@ -1,19 +1,8 @@
 /** @type {import('next').NextConfig} */
 
-const isGithubActions = process.env.GITHUB_ACTIONS || false;
-
-let basePath = "";
-
-if (isGithubActions) {
-  const repo = process.env.GITHUB_REPOSITORY.replace(/.*?\//, "");
-
-  basePath = `/${repo}`;
-}
-
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-  basePath: basePath,
   devIndicators: {
     buildActivity: false,
   },
